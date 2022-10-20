828e4925057722c1442bfa504b8e917cd308731e
paulina-11 <paulinavila.lopez@gmail.com>
2022-10-19 20:47:20 -0500

    crud basico mongo

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import User from './api/models/User.js'
 
 dotenv.config();
 
 mongoose.connect(
-  process.env.DB_URI, () =>{
+  process.env.DB_URI, async() =>{
     console.log('Conexion existosa');
+    // CRUD basico con mongoose
+
+   const user = await User.create({
+      address : 'Jilguero 51',
+      birthday: new Date(),
+      contacts:['Pedrito','Jorge'],
+      email:'correo@correo.com',
+      lastname:'Avila',
+      name:'Paulina',
+      password:'12345',
+      phone:'5678878',
+      role:'Client'
+    });
+  
+    // console.log(user);
+    // read
+   const users = await User.find({
+    name:'Paulina'
+   })
+
+    // update
+    // muchos
+    const promiseResult = await User.updateMany({
+      name:'Juan',
+      lastname:'Rodriguez'
+    })
+    // delete
+    const result = await User.deleteOne({
+      name:'Paulina',
+      lastname:'Avila'
+    },{
+      role:'Client'
+    })
+
+    console.log(result);
+
+
   }
+
+
 );
--- server.js
+++ server.js
@@ -2,5 +2,15 @@ import http from 'http';
 import api from './api/api.js'
 
 const server = http.createServer(api);
+
+server.on('error',(error)=>{
+    if(error.code === 'EADDRINUSE'){
+        console.log('Ese puerto ya esta siendo usado por otra aplicación');
+    }
+    console.error('Error al iniciar servidor ❌ ' + error)
+})
+server.on('listening',()=>{
+    console.log('Servidor escuchando 💚' ); 
+})
 // prender servidor
 server.listen(3000)
