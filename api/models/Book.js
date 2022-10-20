9745f0c96ea7d8369c1d77179a2b12a685dc4ce8
paulina-11 <paulinavila.lopez@gmail.com>
2022-10-19 21:49:55 -0500

    crud book

---
+++ api/models/Book.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  title: String,
+  editorial: String,
+  edition: Number,
+  pageNumber: Number,
+  isbn: String
+});
+
+export default mongoose.model("Book", bookSchema);
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import User from './api/models/User.js'
+import Book from './api/models/Book.js'
 
 dotenv.config();
 
@@ -41,10 +42,36 @@ mongoose.connect(
       role:'Client'
     })
 
-    console.log(result);
+    // console.log(result);
 
+ //  /**
+  //  * 1.- Crear modelo book
+  //  * 2.- create de modelo book
+  //  * 3.- find de Book
+  //  * 4.- Update Book by id
+  //  * 5.- Delete by id
+  //  */
 
-  }
+  const book = await Book.create({
+    title: "La plaza",
+    editorial: "Planeta",
+    edition: 1,
+    pageNumber: 202,
+    isbn: "978-607-07-2236-3"
+  });
+  // console.log(book);
+// Todos los libros
+  const findBook = await Book.find();
 
+console.log(findBook);
 
-);
+const upBook = await Book.findByIdAndUpdate("6350b6a7226bc5ab93b90711", {
+  title: "La plaza2",
+});
+// console.log(upBook);
+
+const deleteBook = await Book.findByIdAndDelete("6350b543e3108f737910c0b2"); 
+const deleteBookTodo = await Book.deleteMany(); 
+
+// console.log(deleteBookTodo);
+  });
